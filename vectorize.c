78ee2da0c0eaf3fbb753b69fb4c0e47c6d424f3a
William Ferreira <wqferr@gmail.com>
2020-12-24 01:03:20 -0300

    Create vector_new for library (not tested!)

--- vectorize.c
+++ vectorize.c
@@ -1,19 +1,53 @@
-#include "lua.h"
-#include "lauxlib.h"
+#include <lauxlib.h>
+#include <lua.h>
+#include <stddef.h>
+#include <stdlib.h>
 
-#define MESSAGE "hello world!"
+const char vector_mt_name[] = "_vectorize_vector_mt";
 
-int return_hello(struct lua_State *L) {
-    lua_pushstring(L, MESSAGE);
-    return 1;
+typedef struct FloatVector {
+  lua_Number *values;
+  size_t len;
+} FloatVector;
+
+int vec_new(lua_State *L) {
+  size_t len;
+  lua_Number *values;
+  FloatVector *v;
+
+  len = luaL_checkinteger(L, 1);
+  if (len <= 0) {
+    luaL_error(L, "Expected positive integer for size, got %d", len);
+  }
+
+  values = malloc(len * sizeof(*values));
+  if (values == NULL) {
+    luaL_error(L, "Could not allocate vector");
+  }
+
+  v = lua_newuserdata(L, sizeof(*v));
+  luaL_setmetatable(L, vector_mt_name);
+  v->len = len;
+  v->values = values;
+  return 1;
 }
 
-static const struct luaL_Reg functions[] = {
-    {"return_hello", return_hello},
-    {NULL, NULL}
-};
+int vec_lib__call(lua_State *L) {
+  // TODO manipulate stack to remove the additional parameter
+  // and call vec_new
+  return 0;
+}
+
+static const struct luaL_Reg functions[] = {{"new", &vec_new}, {NULL, NULL}};
 
 int luaopen_vectorize(lua_State *L) {
-    luaL_newlib(L, functions);
-    return 1;
+  luaL_newmetatable(L, vector_mt_name);
+  luaL_newlib(L, functions);
+
+  lua_pushvalue(L, -1);
+  lua_setfield(L, -3, "__index");
+
+  lua_pushcfunction(L, &vec_lib__call);
+  lua_setfield(L, -3, "__call");
+  return 1;
 }
