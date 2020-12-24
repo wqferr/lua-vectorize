2aec8cd6f881ef6e8156008977abebe0f6039bb9
William Ferreira <wqferr@gmail.com>
2020-12-24 01:21:00 -0300

    Stuff

--- vectorize.c
+++ vectorize.c
@@ -3,7 +3,12 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+// TODO decide if it's going to be 0- or 1-based indexing...
+// leaning towards 1-based to be consistent with the rest of lua
+// maybe let user configure it? Might make it too confusing
+
 const char vector_mt_name[] = "_vectorize_vector_mt";
+const char vector_lib_mt_name[] = "_vectorize_lib_mt";
 
 typedef struct FloatVector {
   lua_Number *values;
@@ -32,22 +37,50 @@ int vec_new(lua_State *L) {
   return 1;
 }
 
-int vec_lib__call(lua_State *L) {
-  // TODO manipulate stack to remove the additional parameter
-  // and call vec_new
+int vec_add(lua_State *L) {
+  lua_pushglobaltable(L);
   return 0;
+}
+
+int vec_lib__call(lua_State *L) {
+  // TODO check if this actually works
+  // this is a macro for a function called rotate, this might make things
+  // more complicated than they seem
+  // UPDATE ON THE TODO:
+  // the positive indices for parameters may be the so called pseudo-indices
+  // it depends on if "upvalues" also include parameters to C functions
+  lua_remove(L, 1);
+  return vec_new(L);
 }
 
 static const struct luaL_Reg functions[] = {{"new", &vec_new}, {NULL, NULL}};
 
-int luaopen_vectorize(lua_State *L) {
+void create_vector_metatable(lua_State *L, int libstackidx) {
   luaL_newmetatable(L, vector_mt_name);
-  luaL_newlib(L, functions);
 
-  lua_pushvalue(L, -1);
-  lua_setfield(L, -3, "__index");
+  lua_pushvalue(L, libstackidx);
+  lua_setfield(L, -2, "__index"); // TODO make __index a function so people can use v[1]
 
+  lua_pushcfunction(L, &vec_add);
+  lua_setfield(L, -2, "__add");
+
+  lua_pop(L, 1);
+}
+
+void create_lib_metatable(lua_State *L) {
+  luaL_newmetatable(L, vector_lib_mt_name);
   lua_pushcfunction(L, &vec_lib__call);
   lua_setfield(L, -3, "__call");
+  lua_pop(L, 1);
+}
+
+int luaopen_vectorize(lua_State *L) {
+  create_lib_metatable(L);
+
+  luaL_newlib(L, functions);
+  luaL_setmetatable(L, vector_lib_mt_name);
+
+  create_vector_metatable(L, -1);
+
   return 1;
 }
