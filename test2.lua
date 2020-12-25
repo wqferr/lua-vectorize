512508e53b95845b4cdb926788a3b5065da9a9a0
William Ferreira <wqferr@gmail.com>
2020-12-25 20:18:03 -0300

    Add some profiling, expose arith ops as funcs

--- test2.lua
+++ test2.lua
@@ -1,12 +1,27 @@
 local vec = require "vectorize"
+local socket = require "socket" -- just for profiling
 
-t = vec.linspace(-1, 1, 21)
-print(t)
-print(t:square())
-print(t:sqrt())
+local v = vec.linspace(-2, 2, 2001)
 
-print()
+local n = 1000
+local f1 = vec.square
+local args1 = {v}
+local f2 = vec.pow
+local args2 = {v, 2}
 
-print(t)
-print(t:cube())
-print(t:cbrt())
+local t1 = socket.gettime()
+for i = 1, n do
+    f1(table.unpack(args1))
+end
+t1 = socket.gettime() - t1
+
+local t2 = socket.gettime()
+for i = 1, n do
+    f2(table.unpack(args2))
+end
+t2 = socket.gettime() - t2
+
+print(n, "iterations")
+print("nop time:", baseline)
+print('f1 time:', t1)
+print('f2 time:', t2)
