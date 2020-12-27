dd3c6f1da499af9f39c39c25113103ad920249cd
William Ferreira <wqferr@gmail.com>
2020-12-27 01:57:03 -0300

    Changes to new-tag.sh

--- new-tag.sh
+++ new-tag.sh
@@ -4,6 +4,8 @@ set -e
 [ -z "$1" ] && { echo 'No tag given'; exit 1; }
 
 git tag -m '' "$1"
-rockspec_file="$(luarocks new_version vectorize-scm-0.rockspec "$1" --tag "$1" | cut -f2 -d' ')"
+scm_rockspec_file='vectorize-scm-0.rockspec'
+rockspec_file="$(luarocks new_version "$scm_rockspec_file" "$1" --tag "$1" | cut -f2 -d' ')"
+luarocks upload --api-key "$(cat ~/.config/luarocks/key)" "$scm_rockspec_file" # TODO proper nightly support
 luarocks upload --api-key "$(cat ~/.config/luarocks/key)" "$rockspec_file"
 git push origin "$1"
