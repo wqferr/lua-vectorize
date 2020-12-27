98f332a6ec9bdfd08254f2147ff47201ae5f1214
William Ferreira <wqferr@gmail.com>
2020-12-27 02:00:22 -0300

    Push tag to origin before uploading rockspec

--- new-tag.sh
+++ new-tag.sh
@@ -4,8 +4,7 @@ set -e
 [ -z "$1" ] && { echo 'No tag given'; exit 1; }
 
 git tag -m '' "$1"
+git push origin "$1"
 scm_rockspec_file='vectorize-scm-1.rockspec'
 rockspec_file="$(luarocks new_version "$scm_rockspec_file" "$1" --tag "$1" | cut -f2 -d' ')"
-luarocks upload --api-key "$(cat ~/.config/luarocks/key)" "$scm_rockspec_file" # TODO proper nightly support
 luarocks upload --api-key "$(cat ~/.config/luarocks/key)" "$rockspec_file"
-git push origin "$1"
