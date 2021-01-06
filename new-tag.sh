782a974ac238a9ced4a90ca64aba3f40572a9a40
William Ferreira <wqferr@gmail.com>
2021-01-06 20:50:57 -0300

    Fix new-tag.sh reference to scm rockspec

--- new-tag.sh
+++ new-tag.sh
@@ -5,6 +5,6 @@ set -e
 
 git tag -m "$2" "$1"
 git push origin "$1"
-scm_rockspec_file='vectorize-scm-1.rockspec'
+scm_rockspec_file='vectorize-scm-2.rockspec'
 rockspec_file="$(luarocks new_version "$scm_rockspec_file" "$1" --tag "$1" | cut -f2 -d' ')"
 luarocks upload --api-key "$(cat ~/.config/luarocks/key)" "$rockspec_file"
