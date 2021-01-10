c5da77a63025767bd9e22057efba7ee61a804d3f
William Ferreira <wqferr@gmail.com>
2021-01-10 19:48:05 -0300

    Add heun to ode

--- ode.lua
+++ ode.lua
@@ -28,9 +28,8 @@ function solver:step()
 end
 
 local function euler_step(f, t, state, cfg, iter)
-  local buf = cfg.buffer
-  state:dup_(buf)
-  local deriv = f(t, buf) or buf
+  state:dup_(cfg.buf)
+  local deriv = f(t, cfg.buf) or cfg.buf
   state:add_(deriv:scale_(cfg.stepsize))
 
   return iter * cfg.stepsize, state
@@ -42,10 +41,46 @@ local function euler(integrand, initstate, stepsize)
   end
   local cfg = {
     stepsize = stepsize,
-    buffer = vec(#initstate)
+    buf = vec(#initstate)
   }
   return custom_solver(euler_step, integrand, initstate, cfg)
 end
 M.euler = euler
+
+local function heun_step(f, t, state, cfg, iter)
+  local buf = cfg.buf
+
+  -- first evaluation
+  state:dup_(buf[1])
+  local deriv_l = f(t, buf[1]) or buf[1] -- derivative at initial point
+  deriv_l:scale_(cfg.stepsize)
+
+  -- intermediate state
+  state:add_(deriv_l, buf[2])
+
+  -- second evaluation
+  local tnext = iter * cfg.stepsize
+  local deriv_r = f(tnext, buf[2]) or buf[2] -- derivative at next point
+  deriv_r:scale_(cfg.stepsize / 2)
+
+  local deriv_avg = deriv_r:psy_(0.5, deriv_l)
+  state:add_(deriv_avg)
+  return tnext, state
+end
+
+local function heun(integrand, initstate, stepsize)
+  if type(initstate) == 'number' then
+    initstate = vec{initstate}
+  end
+  local cfg = {
+    stepsize = stepsize,
+    buf = {
+      vec(#initstate),
+      vec(#initstate)
+    }
+  }
+  return custom_solver(heun_step, integrand, initstate, cfg)
+end
+M.heun = heun
 
 return M
